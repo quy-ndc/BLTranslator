6fc3570416c6c48573a189ae5b933c3b1045d979
Quý <toilachungquy6789@gmail.com>
2024-11-27 21:28:08 +0700

    add some basic UI

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import { View } from 'react-native';
-import Animated, { FadeInUp, FadeOutDown, LayoutAnimationConfig } from 'react-native-reanimated';
-import { Info } from '~/lib/icons/Info';
-import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
 import { Button } from '~/components/ui/button';
 import {
   Card,
@@ -12,84 +9,107 @@ import {
   CardHeader,
   CardTitle,
 } from '~/components/ui/card';
-import { Progress } from '~/components/ui/progress';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
 import { Text } from '~/components/ui/text';
-import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
+import { CloudUpload } from '~/lib/icons/CloudUpload';
+import { Clapperboard } from '~/lib/icons/Clapperboard';
+import { Camera } from '~/lib/icons/Camera';
+import { Video } from '~/lib/icons/Video';
+import { Loader } from '~/lib/icons/Loader';
+import { Textarea } from '~/components/ui/textarea';
+import { Progress } from '~/components/ui/progress';
 
-const GITHUB_AVATAR_URI =
-  'https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg';
 
 export default function Screen() {
-  const [progress, setProgress] = React.useState(78);
 
-  function updateProgressValue() {
-    setProgress(Math.floor(Math.random() * 100));
-  }
+  const [value, setValue] = React.useState('image');
+
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return prev;
+        }
+        return prev + 1;
+      });
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Card className='w-full max-w-sm p-6 rounded-2xl'>
-        <CardHeader className='items-center'>
-          <Avatar alt="Rick Sanchez's Avatar" className='w-24 h-24'>
-            <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
-            <AvatarFallback>
-              <Text>RS</Text>
-            </AvatarFallback>
-          </Avatar>
-          <View className='p-3' />
-          <CardTitle className='pb-2 text-center'>Rick Sanchez</CardTitle>
-          <View className='flex-row'>
-            <CardDescription className='text-base font-semibold'>Scientist</CardDescription>
-            <Tooltip delayDuration={150}>
-              <TooltipTrigger className='px-2 pb-0.5 active:opacity-50'>
-                <Info size={14} strokeWidth={2.5} className='w-4 h-4 text-foreground/70' />
-              </TooltipTrigger>
-              <TooltipContent className='py-2 px-4 shadow'>
-                <Text className='native:text-lg'>Freelance</Text>
-              </TooltipContent>
-            </Tooltip>
-          </View>
+    <View className='flex-1 al p-6 gap-3'>
+      <Tabs
+        value={value}
+        onValueChange={setValue}
+        className='w-full max-w-[400px] mx-auto flex-col gap-1.5'
+      >
+        <TabsList className='flex-row w-full'>
+          <TabsTrigger value='image' className='flex-1'>
+            <Text>Image</Text>
+          </TabsTrigger>
+          <TabsTrigger value='video' className='flex-1'>
+            <Text>Video</Text>
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value='image'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Image</CardTitle>
+              <CardDescription>
+                Decodes gestures in images into insights with precision
+              </CardDescription>
+            </CardHeader>
+            <CardContent className='flex-row justify-between gap-2 native:gap-2'>
+              <Button className='flex-row gap-2 w-1/2' variant={'outline'}>
+                <Camera className='text-foreground' size={17} />
+                <Text>Take Image</Text>
+              </Button>
+              <Button className='flex-row gap-2 w-1/2' variant={'outline'}>
+                <CloudUpload className='text-foreground' size={17} />
+                <Text>Upload Image</Text>
+              </Button>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value='video'>
+          <Card>
+            <CardHeader>
+              <CardTitle>Video</CardTitle>
+              <CardDescription>
+                Give body language gestures from videos a meaning
+              </CardDescription>
+            </CardHeader>
+            <CardContent className='flex-row justify-between gap-2 native:gap-2'>
+              <Button className='flex-row gap-2 w-1/2' variant={'outline'}>
+                <Video className='text-foreground' size={17} />
+                <Text>Film Video</Text>
+              </Button>
+              <Button className='flex-row gap-2 w-1/2' variant={'outline'}>
+                <Clapperboard className='text-foreground' size={17} />
+                <Text>Upload Video</Text>
+              </Button>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+      <Card>
+        <CardHeader>
+          <CardTitle>Result </CardTitle>
         </CardHeader>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Dimension</Text>
-              <Text className='text-xl font-semibold'>C-137</Text>
-            </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Age</Text>
-              <Text className='text-xl font-semibold'>70</Text>
-            </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Species</Text>
-              <Text className='text-xl font-semibold'>Human</Text>
-            </View>
+        <CardContent className='flex-column gap-5 '>
+          <View className='flex-row gap-2 justify-between items-center'>
+            <Loader color='white' className='text-foreground animate-spin' size={18} />
+            <Progress className='w-5/6' value={progress} />
           </View>
+          <Textarea />
         </CardContent>
-        <CardFooter className='flex-col gap-3 pb-0'>
-          <View className='flex-row items-center overflow-hidden'>
-            <Text className='text-sm text-muted-foreground'>Productivity:</Text>
-            <LayoutAnimationConfig skipEntering>
-              <Animated.View
-                key={progress}
-                entering={FadeInUp}
-                exiting={FadeOutDown}
-                className='w-11 items-center'
-              >
-                <Text className='text-sm font-bold text-sky-600'>{progress}%</Text>
-              </Animated.View>
-            </LayoutAnimationConfig>
-          </View>
-          <Progress value={progress} className='h-2' indicatorClassName='bg-sky-600' />
-          <View />
-          <Button
-            variant='outline'
-            className='shadow shadow-foreground/5'
-            onPress={updateProgressValue}
-          >
-            <Text>Update</Text>
-          </Button>
-        </CardFooter>
       </Card>
     </View>
-  );
+  )
 }
---
+++ lib/icons/CloudUpload.tsx
@@ -0,0 +1,4 @@
+import { CloudUpload } from 'lucide-react-native';
+import { iconWithClassName } from './iconWithClassName';
+iconWithClassName(CloudUpload);
+export { CloudUpload };
